3b369258c241e6931449ebd3fe0c6f5891d12cb8
darrenmallett31 <darrenmallett31@googlemail.com>
2017-04-07 11:38:36 +0100

    MOT Table - please see previous notes

--- MOT.sql
+++ MOT.sql
@@ -3,17 +3,12 @@
 CREATE TABLE `MOT` (
   `id` int(11) NOT NULL,
   `asset_id` varchar(10) DEFAULT NULL,
-  `due_date` varchar(10)  DEFAULT NULL,
+  `MOT_due_date` varchar(10)  DEFAULT NULL,
   `notes` text DEFAULT NULL,
   `employee_id` varchar(10) DEFAULT NULL,
+  `supplier` varchar(64)  DEFAULT NULL,
   `booked_date` varchar(64) DEFAULT NULL,
   `booked_garage` varchar(64) DEFAULT NULL,
-  `item_rate` int(11) DEFAULT NULL,
-  `category` varchar(64) DEFAULT NULL,
-  `supplier` varchar(64)  DEFAULT NULL,
-  `supplier_prefix` varchar(64)  DEFAULT NULL,
-  `supplier_nominal_code` varchar(64)  DEFAULT NULL,
-  `item_name` varchar(64)  DEFAULT NULL,
-  `sub_category` varchar(64)  DEFAULT NULL,
-  `image1` LONGBLOB NOT NULL
+  `cost` int(11) DEFAULT NULL,
+  `notes` text  DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
